bfc96a59a0c50ef9905a66f02194842eb4ab3516
bohanssen <bohanssen@gmail.com>
2026-05-18 15:08:34 +0200

    Update DrawioEditor.js

--- js/modules/DrawioEditor.js
+++ js/modules/DrawioEditor.js
@@ -20,13 +20,14 @@ document.addEventListener('alpine:init', () => {
             const width = 'doc_section' in this ? this.doc_section.width : this.section.width;
             const height = 'doc_section' in this ? this.doc_section.height : this.section.height;
             container.innerHTML = `<iframe id="${this.section.id}" frameborder="0" style="width:100%;aspect-ratio: ${width} / ${height};background: transparent;" src="${viewer}"></iframe>`;
-
             const iframe = container.querySelector('iframe');
             iframe.addEventListener('load', () => {
-                iframe.contentWindow.postMessage(JSON.stringify({
-                    action: 'load',
-                    xml: this.currentDiagram
-                }), '*');
+                setTimeout(() => {
+                    iframe.contentWindow.postMessage(JSON.stringify({
+                        action: 'load',
+                        xml: this.currentDiagram
+                    }), '*');
+                }, 100);
             });
         },
 
@@ -43,9 +44,9 @@ document.addEventListener('alpine:init', () => {
             this.editorFrame = container.querySelector('iframe');
             if (this.editorFrame.requestFullscreen) {
                 this.editorFrame.requestFullscreen();
-            } else if (this.editorFrame.webkitRequestFullscreen) { // Safari
+            } else if (this.editorFrame.webkitRequestFullscreen) {
                 this.editorFrame.webkitRequestFullscreen();
-            } else if (this.editorFrame.msRequestFullscreen) { // IE11
+            } else if (this.editorFrame.msRequestFullscreen) {
                 this.editorFrame.msRequestFullscreen();
             }
         },
@@ -62,36 +63,42 @@ document.addEventListener('alpine:init', () => {
         },
 
         setupMessageHandling() {
-          window.addEventListener('message', (evt) => {
-            const allowedSources = [
-              this.editorFrame?.contentWindow,
-              this.$refs.diagram_container?.querySelector('iframe')?.contentWindow
-            ].filter(Boolean);
-        
-            if (!allowedSources.includes(evt.source)) return;
-        
-            let msg = evt.data;
-            if (typeof msg === 'string') {
-              try { msg = JSON.parse(msg); } catch { return; }
-            }
-            if (!msg || typeof msg !== 'object') return;
-        
-            if (msg.event === 'init') {
-              evt.source.postMessage(JSON.stringify({
-                action: 'load',
-                xml: this.currentDiagram
-              }), '*');
-            }
-        
-            if (msg.event === 'save') {
-              this.currentDiagram = msg.xml;
-              this.switchToViewMode();
-            }
-        
-            if (msg.event === 'exit') {
-              this.switchToViewMode();
-            }
-          });
+            const ALLOWED_ORIGIN = 'https://notebook.bohanssen.com';
+
+            window.addEventListener('message', (evt) => {
+                if (evt.origin !== ALLOWED_ORIGIN) return;
+
+                let msg = evt.data;
+                if (typeof msg === 'string') {
+                    try { msg = JSON.parse(msg); } catch { return; }
+                }
+                if (!msg || typeof msg !== 'object') return;
+
+                const editorWindow = this.editorFrame?.contentWindow;
+                const viewerWindow = this.$refs.diagram_container
+                    ?.querySelector('iframe')?.contentWindow;
+
+                const fromEditor = evt.source === editorWindow;
+                const fromViewer = evt.source === viewerWindow;
+
+                if (!fromEditor && !fromViewer) return;
+
+                if (fromEditor && msg.event === 'init') {
+                    evt.source.postMessage(JSON.stringify({
+                        action: 'load',
+                        xml: this.currentDiagram
+                    }), '*');
+                }
+
+                if (fromEditor && msg.event === 'save') {
+                    this.currentDiagram = msg.xml;
+                    this.switchToViewMode();
+                }
+
+                if (fromEditor && msg.event === 'exit') {
+                    this.switchToViewMode();
+                }
+            });
         }
     }));
 });
